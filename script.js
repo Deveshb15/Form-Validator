9070a65de8be1e34ee000cf8e5c609222b97b353
DeveshRB <deveshb583@gmail.com>
2021-01-26 17:54:38 +0530

    Added success page

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+// DOM
+const fname = document.getElementById('fname');
+const lname = document.getElementById('lname');
+const email = document.getElementById('email');
+const phone = document.getElementById('phone');
+
 // Event Listeners
 document.getElementById('fname').addEventListener('blur', validateFName);
 document.getElementById('lname').addEventListener('blur', validateLName);
 document.getElementById('email').addEventListener('blur', validateEmail);
 document.getElementById('phone').addEventListener('blur', validatePhone);
+document.getElementById('sub-btn').addEventListener('click', successPage);
+
 
 
 // Validate Name
@@ -45,18 +53,38 @@ function validateEmail(){
 function validatePhone(){
     const phone = document.getElementById('phone');
     // const re = /^([0-9]{10})$/
-    const re = /^\+?\d{2}??[ ]?\d{10}$/
-
+    // const re = /^\+?\d{2}??[ ]?\[0-9]{10}$/
+    const re = /^(?:(?:\+|0{0,2})91(\s*[\-]\s*)?|[0]?)?[789]\d{9}$/
     if(!re.test(phone.value)){
         phone.classList.add('is-invalid');
     }else{
         phone.classList.remove('is-invalid')
     }
-
 }
 
-document.getElementById('sub-btn').addEventListener('click',(e)=>{
-    document.getElementById('thankyou').style.display = "block";
+// Thank you page
+function validateForm()
+{
+  var fields = ["fname","lname", "email", "phone"]
 
-    e.preventDefault();
-});
+  var i, l = fields.length;
+  var fieldname;
+  for (i = 0; i < l; i++) {
+    fieldname = fields[i];
+    if (document.forms["register"][fieldname].value === "") {
+      alert(fieldname + " can not be empty");
+      return false;
+    }
+  }
+  return true;
+}
+
+function successPage(){
+    if(validateForm()){
+        document.getElementById('changePage').setAttribute("action", "thank.html");
+    }else{
+        validateForm();
+    }
+}
+
+
